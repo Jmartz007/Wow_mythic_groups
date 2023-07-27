af402fa4925ec14fa5a98bc2d8e732769d2145dc
Jmartz007 <jeffrey.martinez87@gmail.com>
2023-07-27 00:54:42 -0800

    role pools

--- mythic_groups_maker.py
+++ mythic_groups_maker.py
@@ -7,16 +7,16 @@ keystone_dict = {
                         "Jmartz": {"level": 16, "dungeon": "Halls of Infusion", "Class": "Warrior", "Role": ["Tank", "DPS"]}},
                 "Cardinal": {"Fluke" : {"level": 14, "dungeon": "Underrot", "Class": "Hunter", "Role": ["DPS"]},
                         "Gael" : {"level": 10, "dungeon": "Brackenhide", "Class": "Druid", "Role": ["DPS"]}},
-                "Sajah": {"Sajah" : {"level": 15, "dungeon": "freehold", "Class": "Druid", "Role": ["Tank, DPS"]},
+                "Sajah": {"Sajah" : {"level": 15, "dungeon": "freehold", "Class": "Druid", "Role": ["DPS"]},
                         "Aythe": {"level": 7, "dungeon": "Vortex Pinnacle", "Class": "Warlock", "Role": ["DPS"]}},
-                "Shelana": {"Shelager" : {"level": 14, "dungeon": "Neltharus", "Class": "Monk", "Role": ["Healer, DPS"]},
-                        "Shelana": {"level": 12, "dungeon": "Halls of Infusion", "Class": "Druid", "Role": ["Healer", "Tank"]}},
+                "Shelana": {"Shelager" : {"level": 14, "dungeon": "Neltharus", "Class": "Monk", "Role": ["Healer"]},
+                        "Shelana": {"level": 12, "dungeon": "Halls of Infusion", "Class": "Shaman", "Role": ["DPS"]}},
                 "Vorrox": {"Ronok" : {"level": 18, "dungeon": "Underrot", "Class": "Warrior", "Role": ["DPS"]},
                         "Vorrox": {"level": 7, "dungeon": "Vortex Pinnacle", "Class": "Demon Hunter", "Role": ["Tank"]},
                         "Xyr" : {"level": 13, "dungeon": "Neltharion's Lair", "Class": "Evoker", "Role": ["Healer", "DPS"]}}
                         }
 
-#%% Classes 
+### Player and Character Classes 
 class Myth_Player:
     def __init__(self, player, char_list):
         self.player_name = player
@@ -81,7 +81,68 @@ for i in keystone_dict:
 # Jmartz.print_character_list() # Print an ind players characters
 # print(Jmartz)
 
-#%% Matchmaking
+
+### Player Pools
+
+def healer_pool(players_list):
+    availablePlayersPool = players_list.copy()
+    healersPool = []
+    print_all_players(availablePlayersPool)
+    for i in availablePlayersPool:
+        print(f"i is: {i}")
+        for x in i.list_of_chars:
+            print(f"x is: {x}")
+            if "Healer" in x.role:
+                healersPool.append(x.char_name)
+                print(f"added {x.char_name} to Healer Pool")
+    return healersPool
+healers = healer_pool(players_list)
+print(healers)
+
+def tank_pool(players_list):
+    availablePlayersPool = players_list.copy()
+    tankPool = []
+    print_all_players(availablePlayersPool)
+    for i in availablePlayersPool:
+        print(f"i is: {i}")
+        for x in i.list_of_chars:
+            print(f"x is: {x}")
+            if "Tank" in x.role:
+                tankPool.append(x.char_name)
+                print(f"added {x.char_name} to Tank Pool")
+    return tankPool
+tanks = tank_pool(players_list)
+print(tanks)
+
+def dps_pool(players_list):
+    availablePlayersPool = players_list.copy()
+    dpsPool = []
+    print_all_players(availablePlayersPool)
+    for i in availablePlayersPool:
+        print(f"i is: {i}")
+        for x in i.list_of_chars:
+            print(f"x is: {x}")
+            if "DPS" in x.role:
+                dpsPool.append(x.char_name)
+                print(f"added {x.char_name} to DPS Pool")
+    return dpsPool
+dpsers = dps_pool(players_list)
+print(dpsers)
+
+
+### Matchmaking
+
+def max_groups(players_list):
+    max_groups = int(len(players_list) / 5)
+    max_tanks = max_groups
+    max_healers = max_groups
+    max_dps = max_groups * 3
+    print(f"\nMax groups: {max_groups}\nMax Tanks: {max_tanks}\nMax_healers: {max_healers}\nMax DPS: {max_dps}")
+    return max_groups, max_tanks, max_healers, max_dps
+
+max_g, max_t, max_h, max_dps = max_groups(players_list)
+
+
 class Group:
     def __init__(self):
         self.group_members = []
@@ -102,26 +163,7 @@ class Group:
             print("Group needs more members")
             self.full_group = False
 
-    def get_tank(self,players_list):
-        for i in players_list:
-            print(f"i is: {i}")
-            for x in i.list_of_chars:
-                print(f"x is: {x}")
-                if "Tank" in x.role:
-                    self.tank.append(x.char_name)
-                    self.group_members.append(x)
-                    players_list.pop(players_list.index(i))
-                    self.group_strings()
-                    self.verify_group()
-                    print("Tank found")
-                    print(self.tank)
-                    break
-                else:
-                    self.verify_group()
-                    print("Tank not found")
-                    return False
-
-    def request_member(self, players_list):
+    """ def request_member(self, players_list):
         if not self.tank:
             self.get_tank(players_list)
         for i in players_list:
@@ -190,69 +232,30 @@ class Group:
                         else:
                             self.verify_group()
                             print("DPS not found")
-        print(f"group members: {self.string_list_of_group_members}")
+        print(f"group members: {self.string_list_of_group_members}") """
             
-    def check_if_role(self, character):
-        if "Tank" in character.role:
-            self.tank.append(character)
-        elif "Healer" in character.role:
-            self.healer.append(character)
-        elif "DPS" in character.role:
-            self.dps.append(character)
-        else: print(f"Role not specified for {character.char_name}")
-
-    def add_group_member(self, character):
-        self.group_members.append(character)
-        Group.verify_group()
-
     def __str__(self):
         return str(self.string_list_of_group_members)
 
-# %% Player Pools
+class AddMembers:
+    '''Parameters need to be edited to accept the tanks list and the player list or character list. Then needs to be able to .pop those character objects out of the player pool
+    '''
+    @staticmethod
+    def get_tank(self,players_list):
+        while len(self.tank) < max_t:
+            for i in tanks:
+                print(f"i is: {i}")
+                self.tank.append(i)
+                self.group_members.append(i)
+                players_list.pop(players_list.index(i))
+                self.group_strings()
+                self.verify_group()
+                print("Tank found")
+                print(self.tank)
 
-def healer_pool(players_list):
-    availablePlayersPool = players_list.copy()
-    healersPool = []
-    print_all_players(availablePlayersPool)
-    for i in availablePlayersPool:
-        print(f"i is: {i}")
-        for x in i.list_of_chars:
-            print(f"x is: {x}")
-            if "Healer" in x.role:
-                healersPool.append(x.char_name)
-                print(f"added {x.char_name} to Healer Pool")
-    return healersPool
-print(healer_pool(players_list))
 
-def tank_pool(players_list):
-    availablePlayersPool = players_list.copy()
-    tankPool = []
-    print_all_players(availablePlayersPool)
-    for i in availablePlayersPool:
-        print(f"i is: {i}")
-        for x in i.list_of_chars:
-            print(f"x is: {x}")
-            if "Tank" in x.role:
-                tankPool.append(x.char_name)
-                print(f"added {x.char_name} to Tank Pool")
-    return tankPool
-print(tank_pool(players_list))
+### Test Functions
 
-def dps_pool(players_list):
-    availablePlayersPool = players_list.copy()
-    dpsPool = []
-    print_all_players(availablePlayersPool)
-    for i in availablePlayersPool:
-        print(f"i is: {i}")
-        for x in i.list_of_chars:
-            print(f"x is: {x}")
-            if "DPS" in x.role:
-                dpsPool.append(x.char_name)
-                print(f"added {x.char_name} to DPS Pool")
-    return dpsPool
-print(dps_pool(players_list))
-
-# %% Test Functions
 def test_new_group():
     print("before groups")
     print_all_players()
