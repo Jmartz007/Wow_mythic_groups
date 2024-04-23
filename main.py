ba7b178706049bbd1d58a9424576d3c63c938f10
Jmartz007 <jeffrey.martinez87@gmail.com>
2024-04-23 01:43:32 -0800

    updated env var config

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ logger.addHandler(stream_handler)
 
 logger.info("STARTED APP ----------")
 
+app = app
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
--- sqlconnector/connect_localconnection.py
+++ sqlconnector/connect_localconnection.py
@@ -10,10 +10,11 @@ load_dotenv()
 user=os.getenv('DB_USERNAME')
 password=os.getenv('DB_PASSWORD')
 database=os.getenv('DB_NAME')
+hostconn=os.getenv('LOCAL_CONNECTION_IP')
 
 
 def local_conn():
-    connection = sqlalchemy.create_engine(f"mysql+pymysql://{user}:{password}@localhost/{database}")
+    connection = sqlalchemy.create_engine(f"mysql+pymysql://{user}:{password}@{hostconn}/{database}")
     # pyconnection = pymysql.connect(host="localhost",
     #                             user=os.getenv('DB_USERNAME'),
     #                             password=os.getenv('DB_PASSWORD'),
