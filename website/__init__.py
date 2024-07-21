f99a4fc234df601bd52bd6cdc5cc983a138ecfde
Jmartz007 <109188512+Jmartz007@users.noreply.github.com>
2024-07-20 20:25:29 -0800

    player delete and character delete buttons work

--- website/__init__.py
+++ website/__init__.py
@@ -1,35 +1,52 @@
+import os
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
-from flask import Flask
 import graypy
+from flask import Flask
 from dotenv import load_dotenv
 
 from sqlconnector.connection_pool import db
 
 load_dotenv()
 
+if not os.path.exists("logs"):
+    os.makedirs("logs", exist_ok=True)
+    print(f"Folder 'logs' created.")
+else:
+    print(f"Folder 'logs' already exists.")
+
 logger = logging.getLogger("main")
 logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)-5s] [%(module)s]-%(funcName)s: %(message)s', datefmt='%b/%d/%y %I:%M:%S %p')
+formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)-5s] [%(module)s.%(funcName)s]: %(message)s', datefmt='%b-%d-%y %I:%M:%S %p')
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
+
+# Create a TimedRotatingFileHandler
+handler = TimedRotatingFileHandler(
+    'logs/WoWapp.log',       # Log file name
+    when='D',         # Rotate at midnight
+    interval=7,              # Rotate every day
+    backupCount=12            # Keep 7 backup files
+)
 
 socket_handler = graypy.GELFTCPHandler(host="localhost", port=5555)
 socket_handler.setLevel(logging.DEBUG)
 socket_handler.setFormatter(formatter)
 logger.addHandler(socket_handler)
 
-logger.info("STARTED APP ----------")
-db = db
+logger.info("---------------------------------")
+logger.info("---------- STARTED APP ----------")
+logger.info("---------------------------------")
+# db = db
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True, template_folder='Templates',static_folder='Static')
     app.config.from_mapping(
         SECRET_KEY='123123123',
-        # DATABASE=os.path.join(app.instance_path, 'website.sqlite'),
     )
 
     if test_config is None:
@@ -39,8 +56,6 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-
-    # a simple page that says hello
     from .views import views
     app.register_blueprint(views, url_prefix="/")
 
--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, make_response, flash, red
 from sqlconnector.sqlReader import *
 from mythicgroupmaker.group_init import main
 from sqlconnector.sqlReader import clear_database
-from sqlconnector.sqlReader import create_dict_from_db, delete_entry
+from sqlconnector.sqlReader import create_dict_from_db, delete_player
 import requests
 import random
 
@@ -14,22 +14,19 @@ logger = logging.getLogger(f"main.{__name__}")
 
 
 
-
 @views.route("/")
 @views.route("/home")
 def home():
     return redirect("/player_entry")
     # s = requests.Session()
-    session.permanent = True
-    if "group id" in session:    
-        randSession = session.get("group id")
-        return render_template("home.html", groupsession=randSession)
-    else:
-       return redirect("/player_entry")
-    
-    return render_template("home.html")
+    # session.permanent = True
+    # if "group id" in session:    
+    #     randSession = session.get("group id")
+    #     return render_template("home.html", groupsession=randSession)
+    # else:
+    #    return redirect("/player_entry")   
+    # return render_template("home.html")
  
-
 @views.route("/player_entry", methods=["GET", "POST"])
 def submit_player():
     if request.method == "POST":
@@ -47,9 +44,6 @@ def submit_player():
         role = request.form.getlist("role")
         logger.debug(role)
         logger.debug(combat_roles)
-
-
-
         if len(role) < 1:
             flash("Please select at least One role", "error")
             return render_template("player_entry.html")
@@ -65,65 +59,26 @@ def submit_player():
     else:
         return render_template("player_entry.html")
 
-@views.route("/admin/delete_players")
-def delete_all_players_prompt():
-    return render_template("delete_players_prompt.html")
-
-
-@views.route("/admin/players_deleted")
-def delete_all_players():
-    clear_database()
-    logger.info("All players deleted")
-    return render_template("tables_deleted.html")
-
-       
-
-@views.route("/error", methods=["GET"])
-def error():
-    return render_template("error.html")
-
-
-
 @views.route("/current_players", methods=["GET", "POST"])
 def current_players():
     if request.method == "POST":
-        CharacterName = request.form.get("characterName")
-        # CharacterName = CharacterName.strip("()'").replace("'", "").split(",")
-        # CharacterName = 
-        print(CharacterName)
-        return render_template("delete_verify.html", CharacterName=CharacterName)
+        data = request.form.to_dict()
+        if data.get("characterName"):
+            CharacterName = request.form.get("characterName")
+            print(CharacterName)
+            return render_template("delete_character.html", CharacterName=CharacterName)
+        else:
+            data.get("playerName")
+            PlayerName = request.form.get("playerName")
+            return render_template("delete_player.html", PlayerName=PlayerName)
     else: 
-        playersDB = read_current_players_db()
-        playersList = []
-
-        if playersDB:
-            for i in playersDB:
-                if i[0] in playersList:
-                    pass
-                else:
-                    playersList.append(i[0])
-            Num_players = len(playersList)
+        playersDB = create_dict_from_db()
+        Num_players = len(playersDB)
+        if Num_players > 0:
             logger.debug(f"Total players: {Num_players}")
             return render_template("current_players.html", playersListDB=playersDB,totalplayers=Num_players)
-
         else:
             return render_template("current_players.html", playersListDB=playersDB, totalplayers=0)
-
-
-
-@views.route("/api/current_players", methods=["GET", "POST"])
-def get_players_from_db():
-
-    if request.method == "POST":
-        pdict = create_dict_from_db()
-        playerName = request.form.get("CharacterName")
-        redirect(url_for('delete_user'))
-        render_template("current_players_api.html", playersListDB=pdict, j=playerName)
-    else:
-        pdict = create_dict_from_db()
-        return render_template("current_players_api.html", playersListDB=pdict, j="None")
-
-
 
 @views.route("/create_groups")
 def create_groups():
@@ -139,36 +94,67 @@ def create_groups():
                 logger.debug(j)
         return render_template("groups_verify.html", groupsList=groupsList, len=length)
 
+@views.route("/delete_entry", methods=["GET", "POST"])
+def delete_user():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        logger.debug(data)
+        if data.get("PlayerName"):
+        # data = data.strip("()'[]").replace("'", "").split(",")
+        # logger.debug(f"data is: {data}")
+        # CharacterName = data[0]
+            PlayerName = data["PlayerName"]
+            logger.debug(f"player to be deleted: {PlayerName}")
+            result = delete_player(PlayerName)
+            return render_template("deleted_user.html", result=result )
+
+        elif data.get("CharacterName"):
+            CharacterName = data["CharacterName"]
+            logger.debug(f"player to be deleted: {CharacterName}")
+            result = delete_character(CharacterName)
+            return render_template("deleted_user.html", result=result )
+    return render_template("delete_entry.html", CharacterName=CharacterName)
+
+# @views.route("/delete_verify", methods=["GET", "POST"])
+# def delete_verify():
+#     if request.method == "POST":
+#         if request.form.getlist("PlayerName"):
+#             data = request.form.getlist("PlayerName")
+#             logger.debug(data)
+#             CharacterName = data[0]
+#             results = delete_player(CharacterName)
+#             return render_template("deleted_user.html", results=results)
+#     return render_template("delete_verify.html")
+
+@views.route("/api/current_players", methods=["GET", "POST"])
+def get_players_from_db():
+    if request.method == "POST":
+        pdict = create_dict_from_db()
+        playerName = request.form.get("CharacterName")
+        redirect(url_for('delete_user'))
+        render_template("current_players_api.html", playersListDB=pdict, j=playerName)
+    else:
+        pdict = create_dict_from_db()
+        return render_template("current_players_api.html", playersListDB=pdict, j="None")
 
 @views.route("/somethingcool")
 def something_cool():
     return render_template("somethingcool.html")
 
 
+@views.route("/admin/delete_players")
+def delete_all_players_prompt():
+    return render_template("delete_players_prompt.html")
 
-@views.route("/delete_entry", methods=["GET", "POST"])
-def delete_user():
-    if request.method == "POST":
-        data = request.form.get("CharacterName")
-        data = data.strip("()'[]").replace("'", "").split(",")
-        logger.debug(f"data is: {data}")
-        CharacterName = data[0]
-        logger.debug(f"player to be deleted: {CharacterName}")
-        result = delete_entry(CharacterName)
-        return render_template("deleted_user.html", result=result )
-    return render_template("delete_entry.html", CharacterName=CharacterName)
-
-
-@views.route("/delete_verify", methods=["GET", "POST"])
-def delete_verify():
-    if request.method == "POST":
-        data = request.form.getlist("CharacterName")
-        logger.debug(data)
-        CharacterName = data[0]
-        results = delete_entry(CharacterName)
-        return render_template("deleted_user.html", results=results)
-    return render_template("delete_verify.html")
-
+@views.route("/admin/players_deleted")
+def delete_all_players():
+    clear_database()
+    logger.info("All players deleted")
+    return render_template("tables_deleted.html")
+       
+@views.route("/error", methods=["GET"])
+def error():
+    return render_template("error.html")
 
 @views.route("/create_session")
 def new_session():
@@ -182,14 +168,12 @@ def new_session():
     # return redirect(url_for('views.submit_player', groupsession=rndNum))
     return redirect(url_for('views.submit_player'))
 
-
 @views.route("/join_session", methods=["POST"])
 def join_session():
     groupid = request.form.get("groupid")
     session["group id"] = groupid
     logger.debug(f"joined session with groupid: {groupid}")
     return redirect(url_for("views.submit_player", groupsession=groupid))
-
 
 @views.route("/cookie", methods=["GET", "POST"])
 def cookies():
@@ -199,12 +183,10 @@ def cookies():
         return render_template("home.html")
     elif "mycookie" in session:
         request.cookies.lists
-        
     else:
         session["group id"] = "007"
         logger.debug("no group id in session cookies")
         return render_template("home.html")
-
 
 @views.route("/set_cookie")
 def set_cookie():
