85b8e4cd6809064ad1f3ebd353f2be6227deebb0
Jmartz007 <jeffrey.martinez87@gmail.com>
2024-08-02 18:44:11 -0800

    logging changes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,3 +3,5 @@ workers = 2
 timeout = 60
 certfile = '/etc/jmartzservegame/jmartz_servegame_com-chain.pem'
 keyfile = '/etc/jmartzservegame/myserver.key'
+accesslog = "-"
+access_log_format = '%(h)s %({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
