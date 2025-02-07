8f269d998cf3740e4b32e0cd25f8b79e6d7eb937
Jmartz007 <109188512+Jmartz007@users.noreply.github.com>
2025-02-06 22:21:28 -0900

    added conditional render on navbar when user is logged in

--- Frontend-React/src/components/NavHeader.tsx
+++ Frontend-React/src/components/NavHeader.tsx
@@ -1,38 +1,43 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <nav className="navbar navbar-expand-lg bg-dark-subtle">
       <NavLink to="/" className="navbar-brand">
         Home
       </NavLink>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink to="/list" className="nav-link">
-            Player List
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/dungeons" className="nav-link">
-            Dungeons
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/new-entry" className="nav-link">
-            New Entry
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/about" className="nav-link">
-            about
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/logout" className="nav-link">
-            Log Out
-          </NavLink>
-        </li>
-      </ul>
+      {isAuthenticated ? (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <NavLink to="/list" className="nav-link">
+              Player List
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/dungeons" className="nav-link">
+              Dungeons
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/new-entry" className="nav-link">
+              New Entry
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" className="nav-link">
+              about
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/logout" className="nav-link">
+              Log Out
+            </NavLink>
+          </li>
+        </ul>
+      ) : null}
     </nav>
   );
 }
