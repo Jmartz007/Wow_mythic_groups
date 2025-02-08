7df205f8833c0e556603c7aef018abe137f3f025
Jmartz007 <109188512+Jmartz007@users.noreply.github.com>
2025-02-08 00:47:28 -0900

    WIP added MUI and login card

--- Frontend-React/src/App.tsx
+++ Frontend-React/src/App.tsx
@@ -11,72 +11,72 @@ import GroupListPage from "./pages/GroupListPage";
 import Logout from "./pages/Logout";
 import RequireAuth from "./components/RequireAuth";
 import { AuthProvider } from "./context/AuthContext";
+import { Container } from "@mui/material";
+// import "./App.css";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="bg-dark">
-        <Navbar />
-        <div className="container p-4">
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-              errorElement={<NotFoundPage />}
-            ></Route>
-            <Route
-              path="/list"
-              element={
-                <RequireAuth>
-                  <PlayersList />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/dungeons"
-              element={
-                <RequireAuth>
-                  <EditDungeons />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/new-entry"
-              element={
-                <RequireAuth>
-                  <PlayerEntry />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/players/:playername"
-              element={
-                <RequireAuth>
-                  <PlayerDetails />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/players/:playername/characters/:charactername"
-              element={
-                <RequireAuth>
-                  <CharacterDetails />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/create-groups"
-              element={
-                <RequireAuth>
-                  <GroupListPage />
-                </RequireAuth>
-              }
-            />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </div>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+            errorElement={<NotFoundPage />}
+          ></Route>
+          <Route
+            path="/list"
+            element={
+              <RequireAuth>
+                <PlayersList />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/dungeons"
+            element={
+              <RequireAuth>
+                <EditDungeons />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/new-entry"
+            element={
+              <RequireAuth>
+                <PlayerEntry />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/players/:playername"
+            element={
+              <RequireAuth>
+                <PlayerDetails />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/players/:playername/characters/:charactername"
+            element={
+              <RequireAuth>
+                <CharacterDetails />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/create-groups"
+            element={
+              <RequireAuth>
+                <GroupListPage />
+              </RequireAuth>
+            }
+          />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Container>
     </AuthProvider>
   );
 }
---
+++ Frontend-React/src/components/LoginCard.tsx
@@ -0,0 +1,111 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { FormEvent } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  FormControl,
+  FormLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+export default function LoginCard() {
+  const navigate = useNavigate();
+  const { isAuthenticated, checkAuthStatus } = useAuth();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.currentTarget);
+      const jsonData = Object.fromEntries(formData.entries());
+
+      const response = await fetch("/groups/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(jsonData),
+      });
+      console.log(JSON.stringify(jsonData));
+
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        console.log(errorResponse);
+        const errorMessage = errorResponse.error.message;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      await checkAuthStatus();
+
+      navigate("/list");
+    } catch (error) {
+      console.error("error submitting form: ", error);
+      if (error instanceof Error) {
+        alert(`Log in error: ${error.message}`);
+      } else {
+        alert("there was an error submitting form");
+      }
+    }
+  };
+  return (
+    <Box
+      sx={{ display: "flex", flexDirection: "column", width: "100%", gap: 2 }}
+    >
+      <Card variant="outlined">
+        {isAuthenticated ? (
+          <CardContent>
+            <div>
+              <h2>Welcome back!</h2>
+              <p>
+                We're glad to see you again. You can navigate to the Player List
+                page to see your players.
+              </p>
+            </div>
+          </CardContent>
+        ) : (
+          <Box component="form" onSubmit={handleSubmit}>
+            <CardContent sx={{ justifyContent: "center" }}>
+              <Typography variant="h3">Log In</Typography>
+              <div className="mb-3">
+                <FormControl>
+                  <FormLabel htmlFor="username" className="form-label">
+                    User Name
+                  </FormLabel>
+                  <TextField
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="User Name"
+                  />
+                </FormControl>
+              </div>
+              <div className="mb-3">
+                <FormControl>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <TextField
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </FormControl>
+              </div>
+            </CardContent>
+
+            <CardActions sx={{ justifyContent: "center", paddingBottom: 2 }}>
+              <Button type="submit" variant="contained">
+                Log In
+              </Button>
+            </CardActions>
+          </Box>
+        )}
+      </Card>
+    </Box>
+  );
+}
