0bbe46a2f7541cf6c064e8c57c087ebf2254c6e5
Jmartz007 <109188512+Jmartz007@users.noreply.github.com>
2025-02-20 00:08:40 -0900

    EntryForm.tsx converted to Material UI components

--- Frontend-React/src/components/EntryForm.tsx
+++ Frontend-React/src/components/EntryForm.tsx
@@ -9,9 +9,17 @@ import {
   InputLabel,
   FormControl,
   FormHelperText,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormGroup,
+  Checkbox,
+  Slider,
+  Typography,
+  useTheme,
 } from "@mui/material";
 import React, { useState, useEffect, FormEvent } from "react";
-
 interface RoleDetails {
   enabled: boolean;
   skill: number;
@@ -34,6 +42,40 @@ type Option = {
   dungeon: string;
 };
 
+const Classes = {
+  Warrior: "darkkhaki",
+  Paladin: "pink",
+  Hunter: "lightgreen",
+  Rogue: "yellow",
+  Priest: "white",
+  Shaman: "royalblue",
+  Mage: "aqua",
+  Warlock: "purple",
+  Monk: "springgreen",
+  Druid: "orange",
+  "Demon Hunter": "darkmagenta",
+  "Death Knight": "red",
+  Evoker: "darkgreen",
+};
+
+const getAdjustedColor = (color: string, isLightTheme: boolean) => {
+  if (isLightTheme) {
+    switch (color) {
+      case "white":
+        return "#000"; // Change white to black for visibility
+      case "yellow":
+        return "#d4a017"; // A darker yellow
+      case "royalblue":
+        return "darkblue";
+      case "darkkhaki":
+        return "saddlebrown";
+      default:
+        return color; // Keep other colors the same
+    }
+  }
+  return color; // No changes for dark theme
+};
+
 const initialFormData = {
   playerName: "",
   characterName: "",
@@ -49,6 +91,8 @@ const initialFormData = {
 export default function EntryForm() {
   const [options, setOptions] = useState<Option[]>([]);
   const [formData, setFormData] = useState<FormData>(initialFormData);
+  const theme = useTheme();
+  const isLightTheme = theme.palette.mode === "light";
 
   useEffect(() => {
     const fetchDungeonOptions = async () => {
@@ -77,11 +121,23 @@ export default function EntryForm() {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handleClassChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    const name = event.target.name;
+    const targetValue = event.target.value;
+    // const value = checked;
+    console.log(`name: ${name}, targetValue: ${targetValue}, value: ${value} `);
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   const handleRoleChange = (
     role: Role,
     key: string,
-    value: string | boolean
+    value: string | boolean | number | number[]
   ) => {
+    console.log(`key: ${key}, value: ${value}`);
     setFormData((prev) => {
       const newRoles = { ...prev.roles };
 
@@ -113,12 +169,13 @@ export default function EntryForm() {
     });
   };
 
-  const toggleRole = (role: Role, checked: boolean) => {
+  const toggleRole = (role: Role, value: boolean) => {
+    console.log(`value: ${value}`);
     setFormData((prev) => ({
       ...prev,
       roles: {
         ...prev.roles,
-        [role]: { ...prev.roles[role], enabled: checked },
+        [role]: { ...prev.roles[role], enabled: value },
       },
     }));
   };
@@ -228,95 +285,93 @@ export default function EntryForm() {
             />
           </Box>
 
-          <label htmlFor="className">Select your class:</label>
-          <div id="className" className="container grid gap-0 row-gap-5">
-            {[
-              "Warrior",
-              "Paladin",
-              "Hunter",
-              "Rogue",
-              "Priest",
-              "Shaman",
-              "Mage",
-              "Warlock",
-              "Monk",
-              "Druid",
-              "Demon Hunter",
-              "Death Knight",
-              "Evoker",
-            ].map((cls) => (
-              <div key={cls}>
-                <input
-                  type="radio"
-                  id={cls.toLocaleLowerCase()}
-                  name="className"
-                  value={cls}
-                  checked={formData.className === cls}
-                  onChange={handleInputChange}
-                />
-                <label htmlFor={cls.toLocaleLowerCase()}>{cls}</label>
-              </div>
+          <Box>
+            <FormControl>
+              <FormLabel id="className">Select your class:</FormLabel>
+              <RadioGroup
+                name="className"
+                onChange={handleClassChange}
+                value={formData.className}
+              >
+                {Object.entries(Classes).map(([cls, clr]) => {
+                  const adjustedColor = getAdjustedColor(clr, isLightTheme);
+                  return (
+                    <FormControlLabel
+                      key={cls}
+                      control={<Radio />}
+                      label={cls}
+                      value={cls}
+                      sx={{ color: adjustedColor }}
+                    />
+                  );
+                })}
+              </RadioGroup>
+            </FormControl>
+          </Box>
+
+          <Box marginBlock={4} minWidth={200} maxWidth={400}>
+            <Typography variant="h6">
+              Select the roles you play with this character
+            </Typography>
+            {(["tank", "healer", "dps"] as Role[]).map((role) => (
+              <Box key={role} marginBlock={2.5}>
+                <FormGroup>
+                  <FormControlLabel
+                    name="role"
+                    id={role}
+                    control={
+                      <Checkbox
+                        checked={formData.roles[role].enabled}
+                        onChange={(e, value) => toggleRole(role, value)}
+                      />
+                    }
+                    label={role.charAt(0).toLocaleUpperCase() + role.slice(1)}
+                  />
+                  <Typography>Skill Level</Typography>
+                  <Slider
+                    step={1}
+                    marks
+                    min={1}
+                    max={5}
+                    name={`${role}-skill`}
+                    value={formData.roles[role].skill}
+                    disabled={!formData.roles[role].enabled}
+                    onChange={(e, value) =>
+                      handleRoleChange(role, "skill", value)
+                    }
+                  />
+
+                  <RadioGroup
+                    name={`combat_role_${role}`}
+                    value={formData.roles[role].combatRole}
+                    onChange={(e, value) =>
+                      handleRoleChange(role, "combatRole", value)
+                    }
+                    row
+                  >
+                    <FormControlLabel
+                      value="Melee"
+                      control={<Radio />}
+                      label="Melee"
+                      disabled={!formData.roles[role]?.enabled}
+                      required={formData.roles[role]?.enabled}
+                    />
+                    <FormControlLabel
+                      value="Ranged"
+                      control={<Radio />}
+                      label="Ranged"
+                      disabled={!formData.roles[role]?.enabled}
+                      required={formData.roles[role]?.enabled}
+                    />
+                  </RadioGroup>
+                </FormGroup>
+              </Box>
             ))}
-          </div>
+          </Box>
 
-          <label>Select the roles you play with this character</label>
-          {(["tank", "healer", "dps"] as Role[]).map((role) => (
-            <div key={role}>
-              <input
-                type="checkbox"
-                id={role}
-                value={role}
-                name="role"
-                checked={formData.roles[role].enabled}
-                onChange={(e) => toggleRole(role, e.target.checked)}
-              />
-              <label htmlFor={role}>
-                {role.charAt(0).toLocaleUpperCase() + role.slice(1)}
-              </label>
-
-              <input
-                type="range"
-                min="1"
-                max="3"
-                id={`${role}-skill`}
-                name={`${role}-skill`}
-                value={formData.roles[role].skill}
-                disabled={!formData.roles[role].enabled}
-                onChange={(e) =>
-                  handleRoleChange(role, "skill", e.target.value)
-                }
-              />
-              <div>
-                <input
-                  type="radio"
-                  id={`${role}-melee`}
-                  name={`combat_role_${role}`}
-                  value="Melee"
-                  disabled={!formData.roles[role]?.enabled}
-                  required={formData.roles[role]?.enabled}
-                  onChange={(e) =>
-                    handleRoleChange(role, "combatRole", e.target.value)
-                  }
-                />
-                <label htmlFor={`${role}-melee`}>Melee</label>
-
-                <input
-                  type="radio"
-                  id={`${role}-ranged`}
-                  name={`combat_role_${role}`}
-                  value="Ranged"
-                  disabled={!formData.roles[role]?.enabled}
-                  required={formData.roles[role]?.enabled}
-                  onChange={(e) =>
-                    handleRoleChange(role, "combatRole", e.target.value)
-                  }
-                />
-                <label htmlFor={`${role}-ranged`}>Ranged</label>
-              </div>
-            </div>
-          ))}
-
-          <Button variant="contained">Submit</Button>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
         </form>
       </Box>
     </Paper>
