60b874a2690b5462a4f3ec5b6cc751035a3a56c5
Jmartz007 <109188512+Jmartz007@users.noreply.github.com>
2025-03-15 19:29:54 -0800

    added raider.io data for created group data

--- Frontend-React/src/components/DataTables/GroupTable.tsx
+++ Frontend-React/src/components/DataTables/GroupTable.tsx
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react";
+import { Player } from "../../types/Player";
+import { Link } from "@mui/material";
+
+interface Group {
+  [playerKey: string]: Player;
+}
+
+interface GroupedPlayers {
+  [groupId: string]: Group;
+}
 
 interface GroupTableProps {
-  data: Array<Record<string, any>>;
+  // data: Array<Record<string, any>>;
+  data: GroupedPlayers;
   onRowClick: (row: Record<string, any>) => void;
 }
 
 const columnMapping: Record<string, string> = {
   char_name: "Character Name",
-  dpsConf: "DPS Confidence",
+  dpsConf: "DPS Skill",
   dungeon: "Dungeon",
-  hConf: "Healer Confidence",
+  hConf: "Healer Skill",
   is_active: "Active",
   key_level: "Key Level",
   playerName: "Player Name",
   range: "Range",
   role: "Role",
-  tConf: "Tank Confidence",
+  tConf: "Tank Skill",
   wow_class: "Class",
+  iLvl: "iLvl",
+  "Highest Key": "Highest Key",
+  profileUrl: "Profile",
 };
 
 const columnOrder: string[] = [
   "char_name",
+  "iLvl",
+  "Highest Key",
   "playerName",
   "wow_class",
   "role",
@@ -30,19 +46,110 @@ const columnOrder: string[] = [
   "dpsConf",
   "hConf",
   "tConf",
+  "profileUrl",
 ];
+
+function renderCellContent(value: any): React.ReactNode {
+  if (value === null || value === undefined) {
+    return "";
+  } else if (typeof value == "string" && value.includes("https://")) {
+    return (
+      <Link href={value} target="_blank">
+        {value}
+      </Link>
+    );
+  }
+  return value.toString();
+}
+
+async function fetchRaiderIo(char_name: string) {
+  const baseUrl = "https://raider.io/api/v1/characters/profile";
+  const params = new URLSearchParams({
+    region: "us",
+    realm: "lightbringer",
+    name: char_name,
+    fields: "gear,mythic_plus_highest_level_runs",
+  });
+  try {
+    const response = await fetch(`${baseUrl}?${params.toString()}`);
+
+    if (!response.ok) {
+      console.error(`Failed to fetch data for ${char_name}`);
+      throw new Error(
+        `Failed to fetch data for ${char_name}: ${response.statusText}`
+      );
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 export default function GroupTable({ data, onRowClick }: GroupTableProps) {
   const [columns, setColumns] = useState<string[]>([]);
+  const [enrichedData, setEnrichedData] = useState(data);
+  const [loading, setLoading] = useState(true);
   const clickableColumns = ["Character Name"];
+
+  useEffect(() => {
+    async function fetchAdditionalData() {
+      const newData = { ...data };
+
+      const fetchPromises: Promise<void>[] = [];
+
+      for (const [groupName, groupData] of Object.entries(newData)) {
+        for (const [charKey, row] of Object.entries(groupData)) {
+          const promise = fetchRaiderIo(String(row.char_name)).then(
+            (apiData) => {
+              if (apiData) {
+                const { gear, profile_url, mythic_plus_highest_level_runs } =
+                  apiData;
+                const { item_level_equipped } = gear;
+                let highestKey = "";
+
+                if (
+                  mythic_plus_highest_level_runs &&
+                  mythic_plus_highest_level_runs.length > 0
+                ) {
+                  const { mythic_level, short_name } =
+                    mythic_plus_highest_level_runs[0];
+                  highestKey = `${short_name} ${mythic_level}`;
+                }
+
+                newData[groupName][charKey] = {
+                  ...row,
+                  profileUrl: profile_url,
+                  iLvl: item_level_equipped,
+                  "Highest Key": highestKey,
+                };
+              }
+            }
+          );
+          fetchPromises.push(promise);
+        }
+      }
+
+      await Promise.all(fetchPromises);
+      setEnrichedData(newData);
+      setLoading(false);
+    }
+
+    fetchAdditionalData();
+  }, [data]);
 
   useEffect(() => {
     setColumns(columnOrder);
   }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="container mt-5 table-responsive-lg">
-      {Object.entries(data).map(([groupName, groupData]) => (
+      {Object.entries(enrichedData).map(([groupName, groupData]) => (
         <div key={groupName}>
           <h2>Group {groupName}</h2>
 
@@ -69,9 +176,7 @@ export default function GroupTable({ data, onRowClick }: GroupTableProps) {
                         clickableColumns.includes(col) ? "clickable-cell" : ""
                       }
                     >
-                      {row[col] !== undefined && row[col] !== null
-                        ? row[col].toString()
-                        : ""}
+                      {renderCellContent(row[col])}
                     </td>
                   ))}
                 </tr>
